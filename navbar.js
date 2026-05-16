106455428281b35dfbc2cfa599625d11168395ad
nedmayo <emayo@macalester.edu>
2026-05-16 10:44:08 -0500

    podcast

--- navbar.js
+++ navbar.js
@@ -23,8 +23,9 @@ function loadNavbar() {
     <div class="nav-links">
         <a href="about.html">About</a>
         <a href="podcast-launch.html">Podcast launch</a>
+        <a href="https://herdstorytelling.substack.com/podcast" target="_blank" rel="noopener">Podcast</a>
         <a href="contact.html">Contact</a>
-        <a href="https://docs.google.com/forms/d/e/1FAIpQLSdUWXf6VWonCT4Zvnoe80TiGM3T9jy9-9vQpV4T13Nl9E9NAg/viewform" target="_blank">Newsletter</a>
+        <a href="https://substack.com/@herdstorytelling" target="_blank" rel="noopener">Newsletter</a>
         <a href="events.html" class="nav-ticket-icon">
             <span class="nav-ticket-label">Tickets</span>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M2 9a3 3 0 0 1 0 6v2a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-2a3 3 0 0 1 0-6V7a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2Z" /><path d="M13 5v2" /><path d="M13 17v2" /><path d="M13 11v2" /></svg>
