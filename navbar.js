1bd4f6e0ae6ab7e4fd9db317dadc60e1d6af1ef1
nedmayo <emayo@macalester.edu>
2026-06-05 13:27:51 -0500

    podcast donate updates

--- navbar.js
+++ navbar.js
@@ -22,9 +22,10 @@ function loadNavbar() {
     <div class="nav-overlay" aria-hidden="true"></div>
     <div class="nav-links">
         <a href="about.html">About</a>
-        <a href="https://herdstorytelling.substack.com/podcast" target="_blank" rel="noopener">Podcast</a>
+        <a href="podcast.html">Podcast</a>
         <a href="contact.html">Contact</a>
         <a href="https://substack.com/@herdstorytelling" target="_blank" rel="noopener">Newsletter</a>
+        <a href="https://checkout.square.site/merchant/MLY9FGP6NR1C1/checkout/HGZGDKXLHGMKMGJYJZVV4CAO" target="_blank" rel="noopener" class="nav-donate-btn">Donate</a>
         <a href="events.html" class="nav-ticket-icon">
             <span class="nav-ticket-label">Tickets</span>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M2 9a3 3 0 0 1 0 6v2a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-2a3 3 0 0 1 0-6V7a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2Z" /><path d="M13 5v2" /><path d="M13 17v2" /><path d="M13 11v2" /></svg>
